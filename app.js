56be5aa3e967e88df8a834a2b42edd37e8ddd8b1
ValentinAyoroa <valentinayoroa@gmail.com>
2022-08-23 16:06:54 -0300

    Register html mobile

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+console.log(__dirname);
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(3000, () => {console.log('Servidor corriendo');});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/register.html'))
+})
