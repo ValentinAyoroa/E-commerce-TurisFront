9fb894c4898c3f579a77e1a8914b449d4041ba1c
Soraya Sanchez <sorayasanchez0503@gmail.com>
2022-08-31 15:37:15 -0500

    inicializacion carrito de compras

--- app.js
+++ app.js
@@ -24,6 +24,14 @@ app.post('/detalle-producto', (req, res) => {
   res.redirect('/')
 })
 
+app.get('/productCart', (req, res) => {
+  res.sendFile(path.join(__dirname, '/src/views/productCart.html'))
+})
+
+app.post('/productCart', (req, res) => {
+  res.redirect('/')
+})
+
 app.listen(3030, () => {
   console.log("Servidor funcionando en el puerto 3030");
 });
