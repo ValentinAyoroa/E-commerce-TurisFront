f8bd880db566f7c2f21a1e8e110c520e94e9957f
rsampaoli <106210672+rsampaoli@users.noreply.github.com>
2022-09-12 21:41:12 -0300

    cambio tamaño imágenes en archivos. cambio tamaño imágenes de producto en index

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -2,7 +2,9 @@
 //PARA QUE DETALLE DE PRODUCTO NO TENGA PROBLEMAS DE RESOLUCIÓN.
 //HAY QUE EDITARLAS, YO LO HICE CON EL PAINT, PONER "CAMBIAR TAMAÑO" Y ASIGNARLO.
 //NO HACERLO POR CÓDIGO.
+
 const productos = require("./data/productos");
+
 controller = {
     home: function (req, res) {
         res.render('index', { productos: productos });
