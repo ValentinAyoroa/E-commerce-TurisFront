216ceac44d5eee0bff2b493305bb45253419d499
rsampaoli <106210672+rsampaoli@users.noreply.github.com>
2022-09-21 18:34:20 -0300

    fix home que no recibia las modificaciones de productos

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,11 +1,16 @@
-//TENEMOS QUE HACER LAS IMÁGENES DE LOS PRODUCTOS DE RESOLUCION EN 600x870 px.
-//PARA QUE DETALLE DE PRODUCTO NO TENGA PROBLEMAS DE RESOLUCIÓN.
-//HAY QUE EDITARLAS, YO LO HICE CON EL PAINT, PONER "CAMBIAR TAMAÑO" Y ASIGNARLO.
-//NO HACERLO POR CÓDIGO.
-const productos = require("../data/productos");
+const fs = require('fs');
+const path = require('path');
+
+function allProducts() {
+    let jsonData = fs.readFileSync(path.join(__dirname, '../data/productos.json'));
+    let data = JSON.parse(jsonData);
+    return data;
+};
+
 controller = {
     home: function (req, res) {
-        res.render('index', { productos });
+        let data = allProducts();
+        res.render('index', { productos: data });
     }
 }
 
