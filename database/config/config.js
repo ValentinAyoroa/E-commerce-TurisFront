fc692d0222bae84a9c5ed1e77bb01d0a87791a9a
unknown <ramirosampaoli@gmail.com>
2022-10-18 22:55:31 -0300

    anotación en config.js de sequelizer

--- database/config/config.js
+++ database/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   "development": {
     "username": "root",
     "password": "Monito123!",
-    "database": "movies_db",
+    "database": "movies_db", //cambiar para otra BD cuándo se necesite
     "host": "127.0.0.1",
     "dialect": "mysql",
     "operatorsAliases": false
