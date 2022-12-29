2acb4292961ee077ffa095576670a976888bde01
Soraya Sanchez <sorayasanchez0503@gmail.com>
2022-12-29 10:29:06 -0500

    Api de usuarios api/users/:id Sprint 8

--- controllers/db_api_controller.js
+++ controllers/db_api_controller.js
@@ -17,6 +17,20 @@ const dbApiController = {
     });
 
     return res.status(200).json({ count: users.length, users: usersMapped });
+  },
+
+  getUsersId: async(req, res) => {
+    const userId = req.params.id;
+    const user = await User.findByPk(userId).catch(error => console.log(error));
+    return res.status(200).json(
+      {
+        id: user.id,
+        firstName: user.first_name,
+        lastName: user.last_name,
+        email: user.email,
+        image: user.avatar,
+        cellphone: user.cellphone
+      });
   }
 };
 
--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -4,4 +4,5 @@ const dbApiController = require('../controllers/db_api_controller');
 const router = express.Router();
 
 router.get('/users', dbApiController.getUsers);
+router.get('/users/:id', dbApiController.getUsersId);
 module.exports = router;
