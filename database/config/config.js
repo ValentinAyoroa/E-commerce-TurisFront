dd78fc310a0d6d1d5c1a866c473334e731820a9f
Soraya Sanchez <sorayasanchez0503@gmail.com>
2023-01-08 17:35:19 -0500

    configurar variables de entorno

--- database/config/config.js
+++ database/config/config.js
@@ -1,9 +1,9 @@
 module.exports = {
   development: {
-    username: 'root',
-    password: '',
-    database: 'turisfront_db', // cambiar para otra BD cuándo se necesite
-    host: '127.0.0.1',
+    username: process.env.USER_DB || 'root',
+    password: process.env.PASSWORD_DB || '',
+    database: process.env.NAME_DB || 'turisfront_db', // cambiar para otra BD cuándo se necesite
+    host: process.env.HOST_DB || '127.0.0.1',
     dialect: 'mysql'
   }
 };
